2b647260e48c5bd01bdc6d186d5793c2b27eb874
antonioramana <antonioramanandraibe@gmail.com>
2024-03-16 19:34:18 +0300

    feat(integer-style): Intégration des styles tailwind css

--- src/modules/auth/pages/login/index.tsx
+++ src/modules/auth/pages/login/index.tsx
@@ -1,9 +1,12 @@
 import logo from "./../../../../assets/logo/rohy.svg";
 import { Input } from "./components";
+import { SemiCircle } from "./components/";
+import { Mode } from "./components/";
 import { useForm } from "react-hook-form";
 import { LoginType } from "./types/login.type";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { loginSchema } from "./schema/login.schema";
+import { useState } from "react";
 
 export const Login = () => {
   const form = useForm<LoginType>({
@@ -20,9 +23,21 @@ export const Login = () => {
     console.log(values);
   };
 
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
     <div className="bg-white2 h-screen grid grid-cols-2">
-      <div className="bg-green"></div>
+      <div className="bg-green relative h-full flex flex-col justify-center items-center text-white">
+        
+         <h1 className="text-4xl font-bold">RohyMalagasy</h1>
+         <p className="text-center">Explore a wealth of information for your success</p>
+         <button className="bg-white text-black px-4 py-2 mt-4 rounded-full">Read More</button>
+        <SemiCircle />
+     </div>
       <div className="flex items-center justify-center">
         <form
           onSubmit={handleSubmit(handleClickSubmit)}
@@ -49,13 +64,24 @@ export const Login = () => {
               error={errors.password?.message}
             />
           </div>
-          <div className=" w-full">
-            <button type="submit" className="bg-green">
-              Get Started
+        <div className="w-full flex flex-col gap-y-4">
+            <div className="flex items-center">
+              <input
+                type="checkbox"
+                id="acceptConditions"
+              />
+              <label htmlFor="acceptConditions" className="ml-2">
+                I accept the <span className="text-green">Terms an conditions</span>
+              </label>
+            </div>
+            
+          <button type="submit" className="bg-green text-white px-4 py-2 rounded-full">
+              GET STARTED
             </button>
-          </div>
+          </div>   
         </form>
       </div>
+      <Mode darkMode={darkMode} toggleDarkMode={toggleDarkMode} />
     </div>
   );
 };
